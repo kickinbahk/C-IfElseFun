adb1edf1c6d8a37c02d797aab722c47c98e88524
kickinbahk@gmail.com <kickinbahk@gmail.com>
2017-04-08 11:30:25 -0700

    if else in C

--- IfElseFun/main.c
+++ IfElseFun/main.c
@@ -9,7 +9,18 @@
 #include <stdio.h>
 
 int main(int argc, const char * argv[]) {
-  // insert code here...
-  printf("Hello, World!\n");
+  int i = 20;
+  int j = 25;
+  int k = ( i > j ) ? 10 : 5;
+  
+  printf("%d\n", k);
+  
+  if ( 5 < j - k ) {
+    printf("The first expression is true.\n");
+  } else if ( j > i ) {
+    printf("The second expression is true\n");
+  } else {
+    printf("Neither expression is true\n");
+  }
   return 0;
 }
